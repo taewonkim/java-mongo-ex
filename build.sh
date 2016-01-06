d895d34adcf397cf1b57642e42f8443d1aba4574
tae1kim <mniktw@gmail.com>
2016-01-06 18:38:54 +0900

    Added build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-@ECHO OFF
+#!/bin/bash
 javac -encoding utf8 -cp .\libs\bson-3.0.4.jar;.\libs\mongodb-driver-3.0.4.jar;.\libs\mongodb-driver-core-3.0.4.jar;. -s .\main -d .\build .\test\Unit1.java
 java -cp .\libs\bson-3.0.4.jar;.\libs\mongodb-driver-3.0.4.jar;.\libs\mongodb-driver-core-3.0.4.jar;.\build Unit1
 
